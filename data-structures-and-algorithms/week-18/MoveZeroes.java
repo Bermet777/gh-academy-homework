3fa10b71ab645c3b863bbf5daa2801362ab2eebd
bermet <bermet.alibek@gmail.com>
2024-08-22 23:30:35 -0400

    update

--- data-structures-and-algorithms/week-18/MoveZeroes.java
+++ data-structures-and-algorithms/week-18/MoveZeroes.java
@@ -1,5 +1,21 @@
-package data-structures-and-algorithms.week-18;
 
-public class MoveZeroes {
-  
+
+class Solution {
+  public void moveZeroes(int[] nums) {
+    
+      int insertPosition = 0;
+      
+    
+      for (int i = 0; i < nums.length; i++) {
+          if (nums[i] != 0) {
+              nums[insertPosition] = nums[i];
+              insertPosition++;
+          }
+      }
+      
+    
+      for (int i = insertPosition; i < nums.length; i++) {
+          nums[i] = 0;
+      }
+  }
 }
