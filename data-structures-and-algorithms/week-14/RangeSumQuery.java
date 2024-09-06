60ee9473437b4f3653d63cf2f0c09cfbfdc19953
bermet <bermet.alibek@gmail.com>
2024-09-06 02:00:24 -0400

    range sum query

--- data-structures-and-algorithms/week-14/RangeSumQuery.java
+++ data-structures-and-algorithms/week-14/RangeSumQuery.java
@@ -1,20 +1,19 @@
 class NumArray {
-
-  class NumArray {
   int[] prefix;
 
-  public NumArray(int[] nums) {
-      int n = nums.length;
-      prefix = new int[n + 1]; 
-      prefix[0] = 0;
-      for (int i = 1; i <= n; i++) {
-          prefix[i] = prefix[i - 1] + nums[i - 1];
+      public NumArray(int[] nums) {
+          int n = nums.length;
+          prefix = new int [n + 1];
+          prefix[0] = 0;
+          for (int i = 1; i <= n; i++) {
+              prefix[i] = prefix[i-1] + nums[i-1];
+          }
+      }   
 
-      }
-  }
-  public int sumRange(int left, int right) {
-      left++; right++;
-
-     return prefix[right] - prefix[left - 1]; 
-  }
+      public int sumRange(int left, int right) {
+          left++; right++;
+          return prefix[right] - prefix[left-1];
+      } 
 }
+
+
