c3338c46ae1619de0aa45474f64d9eb8ab3e1c37
bermet <bermet.alibek@gmail.com>
2025-03-09 23:40:00 -0500

    powX-N  problem solution

--- algorithms/week-19/FibonacciNumber.java
+++ algorithms/week-19/FibonacciNumber.java
@@ -1,3 +1,4 @@
+// brute force solution with exponential time complexity O(2^n), recursive solution (GH)
 class Solution {
   public int fib(int n) {
       if (n <= 1) {
@@ -5,4 +6,57 @@ class Solution {
       }
      return fib(n-1) + fib(n-2);
   }
+}
+
+
+//efficient solution using memoization O(n), space complexity O(n), dynamic programming(GH)
+
+class Solution {
+  private int[] f;
+  private int fib_(int n) {
+      if (n <= 1) return n;
+      if (f[n] == 0) {
+          f[n] = fib_(n-1) + fib_(n-2);
+      }
+      return f[n];
+  }
+  public int fib(int n) {
+      f = new int[n + 1];
+      return fib_(n);
+  }
+}
+
+
+
+// Iterative approach from LC (Time complexity O(n), Space complexity O(1))
+
+class Solution {
+    public int fib(int n) {
+        if (n <= 1) return n;
+        int a = 0, b = 1;
+        while (n-- > 1) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return b;
+    }
+}
+
+
+//Another dynamic programming solution from LC
+
+class Solution 
+{
+    int[] fib_cache = new int[31];
+	
+	public int fib(int N)
+    {
+        if(N <= 1)
+            return N;
+        else if(fib_cache[N] != 0)
+            return fib_cache[N];
+		else 
+            return fib_cache[N] = fib(N - 1) + fib(N - 2);
+    }
 }
---
+++ algorithms/week-19/PowX-N.java
@@ -0,0 +1,16 @@
+//recursive solution GH O(log n) time complexity
+class Solution {
+  public double myPow(double x, int n) {
+      long exponent = (long) n;
+      if (exponent < 0) {
+          exponent = - exponent;
+          x = 1 / x;
+      }
+      return myPow_(x, exponent);
+  }
+  private double myPow_(double x, long exponent) {
+      if (exponent == 0) return 1;
+      return (exponent % 2 == 0) ? myPow_(x * x, exponent / 2)
+                                 : myPow_(x * x, exponent / 2) * x;         
+  }
+}
