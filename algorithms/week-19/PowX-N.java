4ac2a5ddc98b62923715125dbe10dff7847bba08
bermet <bermet.alibek@gmail.com>
2025-03-11 17:34:50 -0500

    alternative solution for pow x problem

--- algorithms/week-19/PowX-N.java
+++ algorithms/week-19/PowX-N.java
@@ -14,3 +14,24 @@ class Solution {
                                  : myPow_(x * x, exponent / 2) * x;         
   }
 }
+
+
+//iterative approach LC solution (time is still O(log(n)) but space is O(1)
+
+class Solution {
+  public double myPow(double x, int n) {
+      if( n < 0) {
+          n = -n;
+          x = 1 / x;
+      }
+      double pow = 1;
+      while (n != 0) {
+          if((n & 1) != 0) {
+              pow = pow * x;
+          }
+          x = x * x;
+          n >>>= 1;
+      }
+      return pow;
+  }
+}
